75f3ede2b90c47b0925b273a2a1970ded7955424
r3drum <mauriciomorales0410@gmail.com>
2020-02-25 16:59:58 -0300

    commit inicial

---
+++ database/migrations/2020_02_25_193709_create_pacientes_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePacientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('rut')->unique();
+            $table->string('nombres');
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno')->nullable();
+            $table->string('direccion')->nullable();
+            $table->string('telefono')->nullable();
+            $table->enum('sexo', ['Masculino', 'Femenino', 'Otro'])->nullable();
+            $table->date('fecha_nacimiento');
+            $table->bigIncrements('ficha_id')->nullable();
+
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacientes');
+    }
+}
