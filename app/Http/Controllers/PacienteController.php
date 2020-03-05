cde8b47681702fb501fa3a825e713c18219352eb
r3drum <mauriciomorales0410@gmail.com>
2020-03-05 07:48:37 -0300

    modal pcte listo

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePaciente;
 use App\Paciente;
 use Illuminate\Http\Request;
 
@@ -38,9 +39,10 @@ class PacienteController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePaciente $request)
     {
         $paciente = new Paciente($request->except('_token'));
+        $paciente->save();
 
         return redirect()->route('paciente.index')->with('success', 'Nuevo Paciente ha sido creado con exito');
     }
